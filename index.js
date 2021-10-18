f58eb6a5ae2e6a334b93dded57fd4e713a48471b
Haksa6 <hakonenweeti@gmail.com>
2021-10-18 10:55:40 +0300

    deploy try

--- index.js
+++ index.js
@@ -23,6 +23,12 @@ app.get('/info', (request, response) => {
     response.send(content)
   })
 })
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
 
 app.get('/api/persons', (request, response) => {
   Person.find().then(persons => {
